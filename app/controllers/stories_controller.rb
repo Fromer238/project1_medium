2957d523ba3c0d8d45494d23de83021487748477
jay <intuitionpz@gmail.com>
2022-12-11 20:23:19 +0800

    add story CRUD

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,46 @@
+class StoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :update, :destroy]
+
+  def index
+    @stories = current_user.stories.order(created_at: :desc)
+  end
+
+  def new
+    @story = current_user.stories.new
+  end
+  
+  def create
+    @story = current_user.stories.new(story_params)
+
+    if @story.save
+      redirect_to stories_path, notice: '故事新增成功'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '故事已更新'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to stories_path, notice: '故事已刪除'
+  end
+  private
+  def find_story
+    @story = current_user.stories.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :content)
+  end
+end
